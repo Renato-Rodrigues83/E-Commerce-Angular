91c8abbd4d6284597121865dfb7d063e718b1233
Renato-Rodrigues83 <renatinprado8832@gmail.com>
2024-06-11 17:08:38 -0300

    last changes

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
-    OnCheckout(){
-        console.log("Checkout");  
-      }
 }
